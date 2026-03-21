a7c0c64e3300b87f4a05bf86507b23abf5bc7957
Owl <mrmoor031305@gmail.com>
2026-03-21 11:06:49 -0500

    i like it with schum pulp

---
+++ Assets/Scripts/PlayerAmmoManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerAmmoManager : MonoBehaviour
+{
+    public int PistolAmmo = 35;
+    public int RifleAmmo = 0;
+    public int ShotgunAmmo = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
