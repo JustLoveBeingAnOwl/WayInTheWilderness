87f9fc4a28cf092843e076b3d1fe9fcb5b69e4c1
Owl <mrmoor031305@gmail.com>
2026-04-01 09:53:12 -0500

    Add ammo variables and weapon manager refs
    Adjust initial pistol ammo and expand PlayerAmmoManager with separate fields for RevolverAmmo, RifleFullAmmo and RifleIntermediateAmmo to support multiple rifle ammo pools. Update RangedWeapons to include a currentMag field and a PlayerAmmoManager reference (ammoManager) for tracking magazine state and linking weapons to the player ammo system.

--- Assets/Scripts/PlayerAmmoManager.cs
+++ Assets/Scripts/PlayerAmmoManager.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class PlayerAmmoManager : MonoBehaviour
 {
-    public int PistolAmmo = 35;
-    public int RifleAmmo = 0;
+    public int PistolAmmo = 28;
+    public int RevolverAmmo = 0;
+    public int RifleFullAmmo = 0;
+    public int RifleIntermediateAmmo = 0;
     public int ShotgunAmmo = 0;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Scripts/RangedWeapons.cs
+++ Assets/Scripts/RangedWeapons.cs
@@ -5,8 +5,10 @@ public class RangedWeapons : MonoBehaviour
     [Header("General Info")]
     public string WeaponName;
     public int MagSize;
+    int currentMag;
     public bool IsAutomatic;
     public AmmoType ammoType;
+    PlayerAmmoManager ammoManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
