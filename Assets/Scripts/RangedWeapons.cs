b6914d075ca916679211d3ccf11a2151b7df406e
Owl <mrmoor031305@gmail.com>
2026-04-02 22:49:20 -0500

    resort the ranged weapons variables, add to Start() and Shoot()

--- Assets/Scripts/RangedWeapons.cs
+++ Assets/Scripts/RangedWeapons.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 public class RangedWeapons : MonoBehaviour
 {
     [Header("General Info")]
-    public string WeaponName;
-    public int MagSize;
-    int currentMag;
-    public bool IsAutomatic;
-    public AmmoType ammoType;
+    public string weaponName;
+    
     PlayerAmmoManager ammoManager;
+    public float Range;
+    [Header("Ammo")]
+    public int magSize;
+    public int currentMag;
+    public AmmoType ammoType;
+    [Header("Fire Rate")]
+    public bool isAutomatic;
+
+    public float rateOfFire;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentMag = magSize;
     }
 
     // Update is called once per frame
@@ -23,6 +29,14 @@ public class RangedWeapons : MonoBehaviour
 
     public void Shoot()
     {
-        
+        if (currentMag > 0)
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+        }
+        else
+        {
+            return;
+        }
     }
 }
