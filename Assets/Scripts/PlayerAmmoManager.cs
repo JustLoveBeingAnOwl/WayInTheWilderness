c073dcdfe62193b3dcf15adf254a541952e8f478
Owl <mrmoor031305@gmail.com>
2026-04-09 22:11:36 -0500

    Create the AddAmmo function to add ammo to the player's weapon.

--- Assets/Scripts/PlayerAmmoManager.cs
+++ Assets/Scripts/PlayerAmmoManager.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class PlayerAmmoManager : MonoBehaviour
 {
+    [Header("Max per Type")]
+    public int MaxPistol = 100;
+    public int MaxRevolver = 60;
+    public int MaxRifleFull = 70;
+    public int MaxRifleIntermediate = 120;
+    public int MaxShotgun = 80;
+    public int MaxSMG = 150;
+    [Header("Ammo Counts")]
     public int PistolAmmo = 28;
     public int RevolverAmmo = 0;
     public int RifleFullAmmo = 0;
@@ -49,7 +57,7 @@ public class PlayerAmmoManager : MonoBehaviour
                 }
                 return ammoReturned;
             case (AmmoType.RifleFull):
-                if((RevolverAmmo -= amount) > 0)
+                if((RifleFullAmmo -= amount) > 0)
                 {
                     
                 }
@@ -57,6 +65,21 @@ public class PlayerAmmoManager : MonoBehaviour
             default:
                 //The only place this function is called is in gun's scripts, but this is a default fallback to return 0 anyway.
                 return ammoReturned;
+        }
+    }
+    //TODO: add it for the other ammo types
+    public void AddAmmo(AmmoType ammo, int amount)
+    {
+        switch (ammo)
+        {
+            case(AmmoType.Pistol):
+                if ((amount+PistolAmmo) <= MaxPistol)
+                {
+                    PistolAmmo = MaxPistol;
+                    return;
+                }
+                PistolAmmo += amount;
+                return;
         }
     }
 }
