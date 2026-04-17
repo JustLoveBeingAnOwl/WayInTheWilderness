c1169f67ed2c6a7a65bed2a34a8c3aabb6a5ca78
Owl <mrmoor031305@gmail.com>
2026-04-16 23:30:31 -0500

    set up creature stats

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class Creature: MonoBehaviour
 {
+    public string Name;
+    public int Health;
+    public int Speed;
+    public int Power;
+    public enum State {Idle, Wandering, Chasing, Attacking}
     void Start()
     {
 
