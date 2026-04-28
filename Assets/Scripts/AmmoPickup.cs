546aebb0e166eced6faf285afaab4a4e531218a4
Owl <mrmoor031305@gmail.com>
2026-04-28 12:21:01 -0500

    pickup

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class AmmoPickup : Item
 {
+    public AmmoType ammoType;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
